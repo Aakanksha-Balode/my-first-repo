2bcddcbb7406caa6381d5238669cac94a135b8e9
Aakanksha-Balode <167279489+Aakanksha-Balode@users.noreply.github.com>
2026-03-28 08:01:22 +0530

    Add files via upload

---
+++ Series_1/Typing_speed_test.py
@@ -0,0 +1,33 @@
+from time import *
+import random as r
+
+def mistake(partest, usertest):
+    error = 0
+    for i in range(len(partest)):
+        try:
+            if partest[i] != usertest[i]:
+                error = error + 1
+        except :
+            error = error + 1
+    return error
+
+def speed_time(time_s, time_e, userinput):
+    time_delay = time_e - time_s
+    time_r = round(time_delay,2)
+    speed = len(userinput)/ time_r
+    return round(speed)
+
+test = [" a paragraph is a self-contained unit",
+        "welcome to my page"]
+
+test1 = r.choice(test)
+print("     ****typing speed****")
+print(test1)
+print()
+print()
+time_1 = time()
+testinput = input("enter : ")
+time_2 = time()
+
+print('Speed : ', speed_time(time_1, time_2, testinput), "w/sec")
+print("error : ", mistake(test1, testinput))
